ee87cac5fd2912a8627f192761dab0c9874ec7c9
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-12-06 13:57:54 -0600

    React files ready, started working on the rows and fetching movies data

---
+++ foobar.js
@@ -0,0 +1,2 @@
+//c2b264dadfec33499cc2744c97dccf81
+//api key
---
+++ netflix/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import request from "./requests";
+function App() {
+  return (
+    <div className="App">
+      <h1>Hey Lets build netflix</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={request.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
